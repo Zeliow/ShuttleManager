b805e4f7299dfbcc05c675914c85d125be4993cb
k.nikolaev <k.nikolaev@ozm.ru>
2026-03-18 10:11:53 +0300

    add field WarningCode

--- ShuttleManager/ShuttleManager.Shared/Models/ShuttleMessages.cs
+++ ShuttleManager/ShuttleManager.Shared/Models/ShuttleMessages.cs
@@ -19,7 +19,7 @@ namespace ShuttleManager.Shared.Models
                 $"BatteryVoltage: {Data.BatteryVoltage_mV / 1000.0:F1}V, " +
                 $"BatteryCharge: {Data.BatteryCharge}%, " +
                 $"Error Code: {Data.ErrorCode}, " +
-                $"Warning Code: {Data.WarningCode}, " +
+                $"Warning Code: {Data.WaringCode}, " +
                 $"StateFlags: {Data.StateFlags}, " +
                 $"ShuttleStatus: {Data.ShuttleStatus}, " +
                 $"ShuttleNumber: {Data.ShuttleNumber}, " +
